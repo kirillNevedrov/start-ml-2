d5b54473032869ca499f9777b9ab38a5872b37c3
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2022-07-13 22:18:32 +0300

    deep learning lesson 3

---
+++ deep_learning/lesson_3/task_9.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+def create_mlp_model():
+    return nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(28 * 28, 256),
+        nn.ReLU(),
+        nn.Linear(256, 10)
+    )
