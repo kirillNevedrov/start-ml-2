e7d50d2aca5816f082c66884e77e1150c5df8e4d
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2022-07-22 00:04:03 +0300

    deep learning lesson 5

--- deep_learning/lesson_5/task_6.py
+++ deep_learning/lesson_5/task_6.py
@@ -1,2 +1,17 @@
+from torchvision.models import alexnet
+from torchvision.models import vgg11
+from torchvision.models import googlenet
+from torchvision.models import resnet18
+
 def get_pretrained_model(model_name: str, num_classes: int, pretrained: bool=True):
-    pass
+    if (model_name == "alexnet"):
+        return alexnet(pretrained=pretrained, num_classes=num_classes)
+
+    if (model_name == "vgg11"):
+        return vgg11(pretrained=pretrained, num_classes=num_classes)
+
+    if (model_name == "googlenet"):
+        return googlenet(pretrained=pretrained, num_classes=num_classes)
+
+    if (model_name == "resnet18"):
+        return resnet18(pretrained=pretrained, num_classes=num_classes)
